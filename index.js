e94563382c23ef3a8eae2cf0e3edde8a95d43cba
talhaali0006969 <111197074+talhaali0006969@users.noreply.github.com>
2022-12-08 15:59:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const DB="mongodb://talha00069:talha00069@ac-dzyauag-shard-00-00.rcrxisk.mongodb
 
 // middleware
 
-const PORT = process.en.PORT || 3000;
+const PORT =process.env;
 const app = express();
 app.use(express.json());
 app.use(authRouter);
